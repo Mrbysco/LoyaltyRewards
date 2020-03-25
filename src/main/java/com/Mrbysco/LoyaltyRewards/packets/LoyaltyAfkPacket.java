b5dbe5137904cc4fe8d50025404f9baeda5a326a
Mrbysco <Mrbysco@users.noreply.github.com>
2020-03-25 17:21:23 +0100

    -

--- src/main/java/com/Mrbysco/LoyaltyRewards/packets/LoyaltyAfkPacket.java
+++ src/main/java/com/Mrbysco/LoyaltyRewards/packets/LoyaltyAfkPacket.java
@@ -1,7 +1,6 @@
 package com.mrbysco.loyaltyrewards.packets;
 
 import com.mrbysco.loyaltyrewards.handlers.AntiAfkHandler;
-
 import io.netty.buffer.ByteBuf;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
