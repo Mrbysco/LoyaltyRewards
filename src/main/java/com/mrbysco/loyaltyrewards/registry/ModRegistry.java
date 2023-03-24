b786d252331d2d2c14771182012147228b50e302
Mrbysco <Mrbysco@users.noreply.github.com>
2023-03-24 15:53:30 +0100

    Port to 1.19.3

--- src/main/java/com/mrbysco/loyaltyrewards/registry/ModRegistry.java
+++ src/main/java/com/mrbysco/loyaltyrewards/registry/ModRegistry.java
@@ -3,7 +3,6 @@ package com.mrbysco.loyaltyrewards.registry;
 import com.mrbysco.loyaltyrewards.Reference;
 import com.mrbysco.loyaltyrewards.reward.RewardRecipe;
 import com.mrbysco.loyaltyrewards.reward.RewardRecipe.RewardRecipeSerializer;
-import net.minecraft.core.Registry;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,7 +11,7 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ModRegistry {
 	public static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, Reference.MOD_ID);
-	public static final DeferredRegister<RecipeType<?>> RECIPE_TYPES = DeferredRegister.create(Registry.RECIPE_TYPE_REGISTRY, Reference.MOD_ID);
+	public static final DeferredRegister<RecipeType<?>> RECIPE_TYPES = DeferredRegister.create(ForgeRegistries.RECIPE_TYPES, Reference.MOD_ID);
 
 	public static final RegistryObject<RecipeType<RewardRecipe>> REWARD_RECIPE_TYPE = RECIPE_TYPES.register("reward_recipe", () -> new RecipeType<>() {
 	});
