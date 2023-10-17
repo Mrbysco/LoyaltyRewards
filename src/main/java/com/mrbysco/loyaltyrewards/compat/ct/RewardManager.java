dc86e0c9605ee4ba19a8c36ad6a8735e24c78257
Mrbysco <Mrbysco@users.noreply.github.com>
2023-10-17 14:31:33 +0200

    Re-enable CraftTweaker support

--- src/main/java/com/mrbysco/loyaltyrewards/compat/ct/RewardManager.java
+++ src/main/java/com/mrbysco/loyaltyrewards/compat/ct/RewardManager.java
@@ -1,63 +1,63 @@
-//package com.mrbysco.loyaltyrewards.compat.ct;
-//
-//import com.blamejared.crafttweaker.api.CraftTweakerAPI;
-//import com.blamejared.crafttweaker.api.action.recipe.ActionAddRecipe;
-//import com.blamejared.crafttweaker.api.annotation.ZenRegister;
-//import com.blamejared.crafttweaker.api.item.IItemStack;
-//import com.blamejared.crafttweaker.api.recipe.manager.base.IRecipeManager;
-//import com.mrbysco.loyaltyrewards.registry.ModRegistry;
-//import com.mrbysco.loyaltyrewards.reward.RewardRecipe;
-//import net.minecraft.core.NonNullList;
-//import net.minecraft.resources.ResourceLocation;
-//import net.minecraft.world.item.ItemStack;
-//import net.minecraft.world.item.crafting.RecipeType;
-//import org.openzen.zencode.java.ZenCodeType;
-//
-//import java.util.Arrays;
-//
-//@ZenRegister
-//@ZenCodeType.Name("mods.loyaltyrewards.RewardManager")
-//public class RewardManager implements IRecipeManager<RewardRecipe> {
-//	public static final RewardManager INSTANCE = new RewardManager();
-//
-//	private RewardManager() {
-//	}
-//
-//	@ZenCodeType.Method
-//	public void addReward(String name, int time, boolean repeatable, IItemStack[] stacks, String[] commands) {
-//		final ResourceLocation id = new ResourceLocation("crafttweaker", name);
-//		final NonNullList<ItemStack> stackList = NonNullList.create();
-//		stackList.addAll(Arrays.stream(stacks).map(IItemStack::getInternal).toList());
-//		final NonNullList<String> commandList = NonNullList.create();
-//		commandList.addAll(Arrays.asList(commands));
-//
-//		final RewardRecipe recipe = new RewardRecipe(id, "", time, repeatable, stackList, commandList);
-//		CraftTweakerAPI.apply(new ActionAddRecipe<>(this, recipe));
-//	}
-//
-//
-//	@ZenCodeType.Method
-//	public void addItemReward(String name, int time, boolean repeatable, IItemStack[] stacks) {
-//		final ResourceLocation id = new ResourceLocation("crafttweaker", name);
-//		final NonNullList<ItemStack> stackList = NonNullList.create();
-//		stackList.addAll(Arrays.stream(stacks).map(IItemStack::getInternal).toList());
-//		final NonNullList<String> commandList = NonNullList.create();
-//		final RewardRecipe recipe = new RewardRecipe(id, "", time, repeatable, stackList, commandList);
-//		CraftTweakerAPI.apply(new ActionAddRecipe<>(this, recipe));
-//	}
-//
-//	@ZenCodeType.Method
-//	public void addCommandReward(String name, int time, boolean repeatable, String[] commands) {
-//		final ResourceLocation id = new ResourceLocation("crafttweaker", name);
-//		final NonNullList<ItemStack> stackList = NonNullList.create();
-//		final NonNullList<String> commandList = NonNullList.create();
-//		commandList.addAll(Arrays.asList(commands));
-//		final RewardRecipe recipe = new RewardRecipe(id, "", time, repeatable, stackList, commandList);
-//		CraftTweakerAPI.apply(new ActionAddRecipe<>(this, recipe));
-//	}
-//
-//	@Override
-//	public RecipeType<RewardRecipe> getRecipeType() {
-//		return ModRegistry.REWARD_RECIPE_TYPE.get();
-//	}
-//}
+package com.mrbysco.loyaltyrewards.compat.ct;
+
+import com.blamejared.crafttweaker.api.CraftTweakerAPI;
+import com.blamejared.crafttweaker.api.action.recipe.ActionAddRecipe;
+import com.blamejared.crafttweaker.api.annotation.ZenRegister;
+import com.blamejared.crafttweaker.api.item.IItemStack;
+import com.blamejared.crafttweaker.api.recipe.manager.base.IRecipeManager;
+import com.mrbysco.loyaltyrewards.registry.ModRegistry;
+import com.mrbysco.loyaltyrewards.reward.RewardRecipe;
+import net.minecraft.core.NonNullList;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeType;
+import org.openzen.zencode.java.ZenCodeType;
+
+import java.util.Arrays;
+
+@ZenRegister
+@ZenCodeType.Name("mods.loyaltyrewards.RewardManager")
+public class RewardManager implements IRecipeManager<RewardRecipe> {
+	public static final RewardManager INSTANCE = new RewardManager();
+
+	private RewardManager() {
+	}
+
+	@ZenCodeType.Method
+	public void addReward(String name, int time, boolean repeatable, IItemStack[] stacks, String[] commands) {
+		final ResourceLocation id = new ResourceLocation("crafttweaker", name);
+		final NonNullList<ItemStack> stackList = NonNullList.create();
+		stackList.addAll(Arrays.stream(stacks).map(IItemStack::getInternal).toList());
+		final NonNullList<String> commandList = NonNullList.create();
+		commandList.addAll(Arrays.asList(commands));
+
+		final RewardRecipe recipe = new RewardRecipe(id, "", time, repeatable, stackList, commandList);
+		CraftTweakerAPI.apply(new ActionAddRecipe<>(this, recipe));
+	}
+
+
+	@ZenCodeType.Method
+	public void addItemReward(String name, int time, boolean repeatable, IItemStack[] stacks) {
+		final ResourceLocation id = new ResourceLocation("crafttweaker", name);
+		final NonNullList<ItemStack> stackList = NonNullList.create();
+		stackList.addAll(Arrays.stream(stacks).map(IItemStack::getInternal).toList());
+		final NonNullList<String> commandList = NonNullList.create();
+		final RewardRecipe recipe = new RewardRecipe(id, "", time, repeatable, stackList, commandList);
+		CraftTweakerAPI.apply(new ActionAddRecipe<>(this, recipe));
+	}
+
+	@ZenCodeType.Method
+	public void addCommandReward(String name, int time, boolean repeatable, String[] commands) {
+		final ResourceLocation id = new ResourceLocation("crafttweaker", name);
+		final NonNullList<ItemStack> stackList = NonNullList.create();
+		final NonNullList<String> commandList = NonNullList.create();
+		commandList.addAll(Arrays.asList(commands));
+		final RewardRecipe recipe = new RewardRecipe(id, "", time, repeatable, stackList, commandList);
+		CraftTweakerAPI.apply(new ActionAddRecipe<>(this, recipe));
+	}
+
+	@Override
+	public RecipeType<RewardRecipe> getRecipeType() {
+		return ModRegistry.REWARD_RECIPE_TYPE.get();
+	}
+}
